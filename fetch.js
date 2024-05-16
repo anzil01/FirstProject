58d0347874a2e709b34f64815b1fa18db337da5d
anzil01 <141616245+anzil01@users.noreply.github.com>
2024-05-16 12:19:40 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,15 +1,18 @@
 // myFetch.js
-
 (function() {
-    // Save reference to original fetch
-    var originalFetch = window.fetch;
-
-    // Override fetch with custom implementation
-    window.fetch = function(url, options) {
-        // Add your custom logic here
-        console.log('Custom fetch implementation is called with URL:', url);
-        
-        // Call original fetch
-        return originalFetch.apply(this, arguments);
-    };
+  window.Promise = () => {}
 })();
+
+// (function() {
+//     // Save reference to original fetch
+//     var originalFetch = window.fetch;
+
+//     // Override fetch with custom implementation
+//     window.fetch = function(url, options) {
+//         // Add your custom logic here
+//         console.log('Custom fetch implementation is called with URL:', url);
+        
+//         // Call original fetch
+//         return originalFetch.apply(this, arguments);
+//     };
+// })();
